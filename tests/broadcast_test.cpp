8b5f12cff698269ec93209a908d6427feff329c9
DevinRShaw <devinrshaw@gmail.com>
2023-10-31 12:35:26 -0700

    broadcast_test finished

--- tests/broadcast_test.cpp
+++ tests/broadcast_test.cpp
@@ -2,6 +2,14 @@
 #include "../client/client.h"
 #include "gtest/gtest.h" //what is wrong with this 
 
+
+#include <iostream>
+#include <cstring>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define MAXBYTES 4096
+
 //runs server so that infinite listen loop doesnt prevent the connectioin test
 void RunServer() {
     Server server;
@@ -17,10 +25,12 @@ void RunServer() {
 class TestClient : public Client {
 public:
     void TestSend() {
-      while (true) {
+      int i = 5;
+      while (i > 0) {
         char testBuffer[] = "Hello, World!";
         SendMessage(testBuffer);
         std::this_thread::sleep_for(std::chrono::seconds(1));
+        i--;
       }
     }
 
@@ -51,7 +61,8 @@ void RunSendClient() {
 TEST(BroadcastTest, Message) {
     TestClient listenClient;
     listenClient.Connect();
-    EXPECT_TRUE(listenClient.TestReceive())
+    EXPECT_TRUE(listenClient.TestReceive());
+    listenClient.Disconnect();
 }
 
 int main(int argc, char **argv) {
