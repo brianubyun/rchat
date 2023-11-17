51f6e7ff9ce3ec7d4893d3163e1c0dd1190e9764
mcuaya1 <mcuay001@ucr.edu>
2023-11-16 23:30:00 -0800

    Finished making unit

--- tests/server_shut_off_test.cpp
+++ tests/server_shut_off_test.cpp
@@ -1,41 +1,74 @@
 #include "../server/server.h"
 #include "../client/client.h"
+#include "../server/commandhandler.h"
+#include <string>
+#include <iostream>
 #include "gtest/gtest.h" //what is wrong with this 
 
-#include <iostream>
+/*possible solution is to create a public variable that checks if server is deleted*/
+/*maybe create test class that overrides handlecommand and addes in a bool to check if deletes*/
 
-class TestServer : public Server {
-public:
-    bool TestShutOffCommand(){
-        //char* input;
-        std::cout<<"Called" << std::endl;
-        while(isRunning){ //While server is running get input
-            //cin.getline("//exit", 10);
-            if(strcmp("//exit", "//exit") == 0){ //If input is exit command
-                //cin.clear();
+//Created a test command handler class with a public bool that is set to true
+//When the //exit command has been called, indicating that the server has been stopped and deleted
+
+class TestCommandHandler : public CommandHandler{
+    public:
+        bool hasBeenDeleted = false;
+        void HandleCommand(char* command, Server* server) {
+            if(strcmp(command, "//exit") == 0){ //If input is exit command
+                std::cin.clear();
                 std::cout<<"Shutting down server" << std::endl;
-                return true;
-                //delete this; //call server destructor 
+                hasBeenDeleted = true;
+                delete server; //call server destructor 
             } 
+            else if (command == "command2") {
+                std::cout << "Executing Command 2" << std::endl;
+                // Add your logic for Command 2
+            } else {
+                std::cout << "Unknown command: " << command << std::endl;
+            }
         }
-        return false;     
-    }
 
 };
 
 
+/*
+class TestServer: public Server{
+    public:
+        bool isRunning = false;
+        TestServer(){
+            std::cout << "Created server" << std::endl;
+        }
+
+        ~TestServer(){
+            isRunning = false;
+        }
+
+        Start(){
+            isRunning = true;
+        }
+
+}
+
+*/
+
+Server* temp_server;
 void RunServer() {
-    Server server;
-    server.Start(); 
+    temp_server = new Server();
+    temp_server->Start(); 
     std::this_thread::sleep_for(std::chrono::seconds(10)); // Sleep for 10 seconds
 }
 
 
 TEST(ServerShutDown, testingServerShutOffCommand) {
-    TestServer server;
-    server.Start(); 
-    std::this_thread::sleep_for(std::chrono::seconds(10)); // Sleep for 10 seconds
-    EXPECT_TRUE(server.TestShutOffCommand());
+    Server* server = new Server();
+    //server->Start();
+    TestCommandHandler commmandhandler;
+    std::string input = "//exit";
+    char command[input.length() + 1];
+    std::strcpy(command, input.c_str());
+    commmandhandler.HandleCommand(command, server);
+    EXPECT_TRUE(commmandhandler.hasBeenDeleted);
 }
 
 
@@ -44,6 +77,5 @@ int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     int result = RUN_ALL_TESTS();
     //serverThread.join();
-
     return result;
 }
