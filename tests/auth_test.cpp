bbdee5a86dac4ddf2a420c1c66f2789edeae0eda
mboyd020 <mboyd020@ucr.edu>
2023-11-20 15:26:43 -0800

    created several more tests

--- tests/auth_test.cpp
+++ tests/auth_test.cpp
@@ -14,6 +14,7 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <sstream>
+#include <fstream>
 using namespace std;
 TEST(userTests, username/password)
 {
@@ -32,5 +33,39 @@ TEST(userTests, getCred)
     User user;
     user = usercred.GetUser();
     ASSERT_TRUE(user.GetUsername().compare("test"));
-    ASSErt_TRUE(user.GetPassword().compare("-841574"));
+    ASSERt_TRUE(user.GetPassword().compare("-841574"));
+}
+TEST(serverAuthTests, isUserUsernameOnlyTrue)
+{
+    ofstream out("users.txt");
+    out << "test4700203200\n";
+    ServerAuthenticator auth;
+    ASSERT_TRUE(auth.isUser(to_string("test")));
+}
+TEST(serverAuthTests, isUserUsernameOnlyFalse)
+{
+    ofstream out("users.txt");
+    out << "test4700203200\n";
+    ServerAuthenticator auth;
+    ASSERT_TRUE(!auth.isUser(to_string("fake")));
+}
+TEST(serverAuthTests, isUserUsernameAndPasswordTrue)
+{
+    ofstream out("users.txt");
+    out << "test4700203200\n";
+    ServerAuthenticator auth;
+    ASSERT_TRUE(auth.isUser("test4700203200\n"));
+}
+TEST(serverAuthTests, isUserUsernameAndPasswordFalse)
+{
+    ofstream out("users.txt");
+    out << "test4700203200\n";
+    ServerAuthenticator auth;
+    ASSERT_TRUE(auth.isUser("test4700202200\n"));
+}
+TEST(serverAuthTests, writeUser)
+{
+    ServerAuthenticator auth;
+    auth.writeUser("test\n");
+    ASSERT_TRUE(auth.isUser("test\n"));
 }
