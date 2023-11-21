c8d24f0f8d113c3371cf0267dd2217fcbf051b58
Strickvax <strickvax@gmail.com>
2023-11-21 00:00:22 -0800

    Increase input buffer

--- client/client_command_handler.h
+++ client/client_command_handler.h
@@ -11,17 +11,19 @@ public:
     void ListenFor(Client* client){
         char* input;
         while(client->isRunning){ //While client is running get input
-          std::cin.getline(input, 10);
-          HandleCommand(input, client);
+          std::cin.getline(input, 100);
+          HandleCommand(input, client); 
           std::cin.clear();
         }   
     }
 
-     void HandleCommand(char* command, Client* server) {
+     void HandleCommand(char* command, Client* client) {
          if(strcmp(command, "//exit") == 0) { //If input is exit command
             std::cin.clear();
             std::cout<<"Shutting down client" << std::endl;
             delete Client; //call client destructor 
+            std::cin.clear(); //Clears buffer
+          }
      }
 };
 
