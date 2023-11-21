ecc11a76ce151b42b5d8ac1d07a413c1b446777d
Strickvax <strickvax@gmail.com>
2023-11-21 00:23:35 -0800

    Replace commands with emojis within messages

--- client/emojis.h
+++ client/emojis.h
@@ -2,17 +2,28 @@
 #define EMOJI_H
 
 #include <string>
+#include <algorithm>
 
 class Emoji {
-    std::string getEmoji(std::string command)
-    {
-        if (command == "//test")
-            return ("=w=");
+public:
+    // function that actually processes the message
+    // it unfortunately checks EVERY emoji EVERY message it sends
+    // TODO: Only have it call the function if // is used
+    std::string ProcessMessage(std::string message) {
+        ReplaceCommandWithEmoji(message, "//test", "=w=");
+        ReplaceCommandWithEmoji(message, "//test2", "=______=");
+        return message;
+    }
 
-        if (command == "//test2")
-            return ("=______=");
-        
-        return "";
+private:
+    // message, command, and emoji
+    void ReplaceCommandWithEmoji(std::string& message, const std::string& command, const std::string& emoji) {
+        int position = 0;
+        // npos is not-position, but C++ can also interpret it to mean [end of a string]
+        while ((position = message.find(command, position)) != std::string::npos) {
+                message.replace(position, command.length(), emoji);
+                position += emoji.length();
+        }
     }
 };
 
