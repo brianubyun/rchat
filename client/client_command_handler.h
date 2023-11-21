2083a65a09f59db64d3d01681c243aef186129dc
Strickvax <strickvax@gmail.com>
2023-11-20 19:05:30 -0800

    Start transferring exit command to handler

--- client/client_command_handler.h
+++ client/client_command_handler.h
@@ -8,7 +8,21 @@
 
 class ClientCommandHandler {
 public:
-    
+    void ListenFor(Client* client){
+        char* input;
+        while(client->isRunning){ //While client is running get input
+          std::cin.getline(input, 10);
+          HandleCommand(input, client);
+          std::cin.clear();
+        }   
+    }
+
+     void HandleCommand(char* command, Client* server) {
+         if(strcmp(command, "//exit") == 0) { //If input is exit command
+            std::cin.clear();
+            std::cout<<"Shutting down client" << std::endl;
+            delete Client; //call client destructor 
+     }
 };
 
 #endif 
