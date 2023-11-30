5eff2db9983072c7ead62d5286d0db87c0ce9c3e
brianubyun <brianb9658@gmail.com>
2023-11-29 23:04:05 -0800

    modified user.cpp so lines are not bunched up together

--- client/user/user.cpp
+++ client/user/user.cpp
@@ -21,16 +21,19 @@ std::string User::GetUsername() {
 
 std::string User::GetPassword() {
     long long int hashedPassword = 0;
-    for(char i : password){
+    for(char i : password) {
         hashedPassword += (i * 10) % 177;
         hashedPassword += i;
         hashedPassword *= 3;
     }
+
     srand(hashedPassword);
     unsigned long long randNum = (rand() % (1000) * rand() % (1000) * rand() % (1000) * rand() % (1000) * rand() % (1000));
     hashedPassword *= randNum;
+
     srand(hashedPassword);
     randNum = (rand() % (1000) * rand() % (1000) * rand() % (1000) * rand() % (1000) * rand() % (1000));
     hashedPassword *= randNum;
+    
     return std::to_string(hashedPassword);
 }
