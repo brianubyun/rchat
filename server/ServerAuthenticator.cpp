cf408d93f40b23a2cbbf1b4353d495b4c963e694
brianubyun <brianb9658@gmail.com>
2023-11-29 23:47:59 -0800

    cleaned up files

--- server/ServerAuthenticator.cpp
+++ server/ServerAuthenticator.cpp
@@ -9,50 +9,47 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <algorithm>
+
 #include "ServerAuthenticator.h"
 //#define MAXBYTES 4096
-using namespace std;
 
-const bool ServerAuthenticator::isUser(std::string username)
-{
+const bool ServerAuthenticator::isUser(std::string username) {
+    //Opens .txt file
     in.open("users.txt");
-    if(!in.is_open())
-    {
+    if (!in.is_open()) { //Stops if failure to open file
         abort;
     }
-    string curr;
-    while(getline(in, curr))
-    {
-        if(!curr.substr(0, curr.find(breakChar)).compare(username))
-        {
-            in.close();
+
+    std::string curr;
+    while (getline(in, curr)) {
+        if (!curr.substr(0, curr.find(breakChar)).compare(username)) { //Username is found when compared to specific format of username stored in file
+            in.close(); //Closes file since username is found
             return true;
         }
     }
+
+    //Username not found
     in.close();
     return false;
 }
 
-const bool ServerAuthenticator::isUser(char message[])
-{
+const bool ServerAuthenticator::isUser(char message[]) {
     in.open("users.txt");
-    if(!in.is_open())
-    {
+    if (!in.is_open()) {
         abort;
     }
-    string userAndPass(message);
-    string curr;
-    //puts the string in the right format
-    for(int i = 0; i < (userAndPass.length() - 1); ++i)
-    {
+
+    std::string userAndPass(message);
+    std::string curr;
+    //Puts the string in the right format
+    for (int i = 0; i < (userAndPass.length() - 1); ++i) {
         userAndPass.at(i) = userAndPass.at(i+1);
     }
     userAndPass.pop_back();
-    while(getline(in, curr))
-    {
+
+    while (getline(in, curr)) {
         in.ignore();
-        if(!curr.compare(userAndPass))
-        {
+        if (!curr.compare(userAndPass)) {
             //in.close();
             return true;
         }
@@ -61,24 +58,22 @@ const bool ServerAuthenticator::isUser(char message[])
     return false;
 }
 
-bool ServerAuthenticator::writeUser(char message[])
-{
-    out.open("users.txt", ios::app);
-    if(!out.is_open())
-    {
+bool ServerAuthenticator::writeUser(char message[]) {
+    out.open("users.txt", std::ios::app);
+    if (!out.is_open()) {
         abort;
     }
-    string userAndPass(message);
-    for(int i = 0; i < (userAndPass.length() - 1); ++i)
-    {
+
+    std::string userAndPass(message);
+    for (int i = 0; i < (userAndPass.length() - 1); ++i) {
         userAndPass.at(i) = userAndPass.at(i+1);
     }
     userAndPass.pop_back();
     userAndPass.append("\n");
+
     //searches for the username to see if the user already exists
-    string username = userAndPass.substr(0, userAndPass.find(breakChar));
-    if(!isUser(username))
-    {
+    std::string username = userAndPass.substr(0, userAndPass.find(breakChar));
+    if (!isUser(username)) {
         out << userAndPass;
         out.close();
         return true;
@@ -87,9 +82,9 @@ bool ServerAuthenticator::writeUser(char message[])
     return false;
 }
 
-bool ServerAuthenticator::authUser(int clientSocket)
-{
+bool ServerAuthenticator::authUser(int clientSocket) {
     char buffer [4096];
+
     while (true) {
         ssize_t bytesReceived = recv(clientSocket, buffer, sizeof(buffer), 0);
         if (bytesReceived <= 0) {
@@ -97,24 +92,33 @@ bool ServerAuthenticator::authUser(int clientSocket)
             //break;
             return false;
         }
+
         buffer[bytesReceived] = '\0'; // Ensure null-termination
-        if(buffer[0] == '1')
-        {
+        if (buffer[0] == '1') {
             bool isuser = isUser(buffer);
-            char message [2];
-            if(isuser){message[0] = '1';}
-            else{message[0] = '0';}
+            char message[2];
+            if (isuser) {
+                message[0] = '1';
+            }
+            else {
+                message[0] = '0';
+            }
             message[1] = '\0';
+
             send(clientSocket, message, 2, 0);
             return isuser;
         }
-        else
-        {
+        else {
             bool writeuser = writeUser(buffer);
             char message [2];
-            if(writeuser){message[0] = '1';}
-            else{message[0] = '0';}
+            if (writeuser) {
+                message[0] = '1';
+            }
+            else {
+                message[0] = '0';
+            }
             message[1] = '\0';
+
             send(clientSocket, message, 2, 0);
             return writeuser;
         }
--- server/ServerAuthenticator.h
+++ server/ServerAuthenticator.h
@@ -11,10 +11,12 @@ class ServerAuthenticator
         std::ifstream in;
         const char breakChar = 17;
         const char nlChar = 10;
+
     public:
         const bool isUser (std::string username);
         const bool isUser (char[]);
         bool writeUser (char[]);
         bool authUser (int);
 };
+
 #endif
