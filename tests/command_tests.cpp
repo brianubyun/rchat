7a742deedc32623c1da135075e7ae69a6e2c41d9
Strickvax <strickvax@gmail.com>
2023-12-04 17:47:29 -0800

    Start working on unit tests

--- tests/command_tests.cpp
+++ tests/command_tests.cpp
@@ -1,23 +1,29 @@
 #include "../client/client.h"
 #include "../server/server.h"
+#include "../client/emojis.h"
+#include "../client/client_command_handler.h"
 #include "gtest/gtest.h"
 
 TEST(CommandTest, standaloneEmoji) {
-    char buffer[] = "//test";
-    EXPECT_EQ();
+    std::string expected = ":)";
+    std::string message = "//smile";
+    EXPECT_EQ(expected, Emoji::ProcessMessage(message));
 }
 
 TEST(CommandTest, emojiWithinText) {
-    char buffer[] = "This is a //test, right?";
-    EXPECT_EQ();
+    std::string expected = "This is a :), right?";
+    std::string message = "This is a //smile, right?";
+    EXPECT_EQ(expected, Emoji::ProcessMessage(message));
 }
 
 TEST(CommandTest, doubleEmoji) {
-    char buffer[] = "//test//test2";
-    EXPECT_EQ();
+    std::string expected = ":):(";
+    std::string message = "//smile//sad";
+    EXPECT_EQ(expected, Emoji::ProcessMessage(message));
 }
 
 TEST(CommandTest, exitCommand) {
-    char buffer[] = "//exit";
-    EXPECT_EQ();
+    Client dummyClient;
+    std::string message = "//exit";
+    EXPECT_FALSE(ClientCommandHandler::HandleCommand(message, &dummyClient));
 }
