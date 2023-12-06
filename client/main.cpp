9ff489344aea68afb7028738dba19cb1c9878f52
brianubyun <brianb9658@gmail.com>
2023-12-05 22:33:05 -0800

    organized emojis and client main

--- client/main.cpp
+++ client/main.cpp
@@ -2,22 +2,13 @@
 #include <iostream>
 
 int main() {
-    // Create a client instance
     Client client;
 
-    
-    // Connect to the server
     if (!client.Connect()) {
         std::cerr << "Failed to connect to the server." << std::endl;
         return 1;
     }
 
-    // Start the client (send and receive messages)
-    client.Start();
-
-    // The Start function will block until both send and receive threads finish.
-
-    // You can add additional logic here if needed.
-    
+    client.Start(); 
     return 0;
 }
